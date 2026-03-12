456736fef8e87fae4ee29110edd0acd8579c18ff
JustAVirus <91348480+NotJustAVirus@users.noreply.github.com>
2026-03-12 10:48:33 +0100

    Refactor product models to extend a base Product class for better code reuse
    Co-authored-by: PausedMagician <PausedMagician@users.noreply.github.com>

---
+++ backend/src/models/Product.ts
@@ -0,0 +1,21 @@
+import { ProductType } from "./ProductType";
+
+export class Product {
+  public title: string;
+  public imageUrl: string;
+  public basePrice: number;
+  public taxRate: number = 1.25;
+  public discountRate: number = 0.15;
+  public productType: ProductType;
+
+  constructor(productType: ProductType, title: string, imageUrl: string, basePrice: number) {
+    this.productType = productType;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.basePrice = basePrice;
+  }
+
+  public getPrice(): number {
+    return (this.basePrice * (1 - this.discountRate)) * this.taxRate;
+  }
+}
